933d5a43a471e8205aa013da61e76d19df8c64c5
kauanoronha <97698851+kauanoronha@users.noreply.github.com>
2024-04-25 15:42:51 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function calcularParcelas() {
+    var valorTotal = document.getElementById('valorTotal').value;
+    var tabela = document.getElementById('tabelaParcelamento').getElementsByTagName('tbody')[0];
+    tabela.innerHTML = ''; // Limpa a tabela
+
+    for (var i = 1; i <= 10; i++) {
+        var valorParcela;
+        if (i < 4) {
+            valorParcela = valorTotal / i;
+        } else {
+            var taxaJuros = 0.03; // 3% de juros
+            var montante = valorTotal * Math.pow(1 + taxaJuros, i - 3);
+            valorParcela = montante / i;
+        }
+
+        // Insere a linha na tabela
+        var novaLinha = tabela.insertRow();
+        var celulaParcela = novaLinha.insertCell(0);
+        var celulaValorParcela = novaLinha.insertCell(1);
+        var celulaValorTotal = novaLinha.insertCell(2);
+
+        celulaParcela.innerHTML = i + 'x';
+        celulaValorParcela.innerHTML = valorParcela.toFixed(2);
+        celulaValorTotal.innerHTML = (valorParcela * i).toFixed(2);
+    }
+}
